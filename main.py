73a38833d27594a4b72cee21b6b8c54ccb3aa695
Nikita <fedorovni1996@yandex.ru>
2019-10-06 18:39:47 +0500

    Добавил url, количество комнат и почистил данные

--- main.py
+++ main.py
@@ -11,34 +11,30 @@ def get_html(url):
         proxy = {'https': 'https://' + choice(proxies)}
         useragent = {'User-Agent': choice(useragents)}
         print(proxy, useragent)
-        # r = requests.get(url, headers=useragent, proxies=proxy)
-        # print(r.status_code)
-        # if r.status_code == 200:
-        #     print(200, 'work')
-        #     sleep(uniform(1,3))
-        #     return r.text
-        # else:
-        #     print(r.status_code)
-        #     print(404, 'dont work')
-
         try:
             r = requests.get(url, headers=useragent, proxies=proxy)
             print(200, 'work')
             sleep(uniform(1,3))
             return r.text
         except:
-            print(404, 'dont work')
+            print('dont work')
             continue
 
 def get_data(html):
     soup = BeautifulSoup(html, 'lxml')
     links = soup.find_all('a', class_='_3_q0k')
     for link in links:
-        price = link.find('div', class_='_2LpFR').text.strip()
+        url = 'https://domclick.ru' + link.get('href')
+        pr = link.find('div', class_='_2LpFR').text.strip()
+        price = ''.join(pr.split(' ')[:-1])
         adress = link.find('p', class_='_20lRQ').text.strip()
-        square = link.find_all('li', class_='_2aoTm')[1].text
-        floor = link.find_all('li', class_='_2aoTm')[2].text
-        print(price, adress, square, floor)
+        rooms = link.find_all('li', class_='_2aoTm')[0].text
+        sq = link.find_all('li', class_='_2aoTm')[1].text
+        square = sq.split(' ')[0]
+        fl = link.find_all('li', class_='_2aoTm')[2].text
+        floor = fl.split(' ')[0]
+        max_floor = fl.split(' ')[-1]
+        print(url, 'Количество комнат:', rooms,'Цена:', price, 'Адрес:', adress, 'Площадь:', square, 'Этаж:', floor, 'из', max_floor)
 
 def main():
     url = 'https://domclick.ru/search?address=f2b23f98-35d8-4d18-a433-7a01e7820cf5&offset=0&limit=30&category=living&deal_type=sale&has_photo=1&offer_type=flat&rooms=1&sort_dir=desc'
